2810cf5bf3669fa422550bf9cb2d230390d7ea2c
Leandro Guilhermiti <lguilhermiti@gmail.com>
2025-01-28 21:50:23 -0300

    first commit, app-class and index

---
+++ gallery/app-class.js
@@ -0,0 +1,75 @@
+function getElement(selection) {
+	const element = document.querySelector(selection);
+	if (element) {
+		return element;
+	}
+	throw new Error(
+		`Please check "${selection}" selector, no sush element exists`
+	);
+}
+
+class Gallery {
+	constructor(element) {
+		this.container = element;
+		this.list = [...element.querySelectorAll('.img')];
+		this.modal = getElement('.modal'); 
+		this.modalImg = getElement('.main-img');
+		this.imageName = getElement('.image-name');
+		this.modalImages = getElement('.modal-images');
+		this.closeBtn = getElement('.close-btn');
+		this.nextBtn = getElement('.next-btn');
+		this.prevBtn = getElement('.prev-btn');
+
+		this.closeModal = this.closeModal.bind(this);
+		this.nextImage = this.nextImage.bind(this);
+		this.prevImage = this.prevImage.bind(this);
+		this.chooseImage = this.chooseImage.bind(this);
+
+		//container event
+		this.container.addEventListener(
+			'click',
+			function (e) {
+				if (e.target.classList.contains('img')) {
+					this.openModal(e.target, this.list);
+				}
+			}.bind(this)
+		);
+	}
+	openModal(selectedImage, list) {
+		this.setMainImage.innerHTML = list
+			.map(function(image) {
+				return `<img src="${image.src}"
+				title="${image.title}" data-id="${image.dataset.id}"
+				class="${selectedImage.dataset.id === image.dataset.id ? 'modal-img selected' : 'modal-img'}"
+				/>`;
+			})
+			.join('');
+		this.modal.classList.add('open');
+		this.closeBtn.addEventListener('click', this.closeModal);
+		this.nextBtn.addEventListener('click', this.nextImage);
+		this.prevBtn.addEventListener('click', this.prevImage);
+		this.modalImages.addEventListener('click', this.chooseImage);
+	}
+
+
+	setMainImage(selectedImage) {
+		this.modalImg.src = selectedImage.src;
+		this.imageName.textContent = selectedImage.title;
+	}
+
+	closeModal() {
+		this.modal.classList.remove('open');
+		this.closeBtn.removeEventListener('click', this.closeModal);
+		this.nextBtn.removeEventListener('click', this.nextImage);
+		this.prevBtn.removeEventListener('click', this.prevImage);
+		this.modalImages.removeEventListener('click', this.chooseImage);
+	}
+
+	nextImage() {
+		
+	}
+
+
+
+
+}
